be428e7257ef89872fb5150f64c615fd7ea277cf
vsoch <vsochat@stanford.edu>
2016-08-02 16:27:41 -0700

    adding gravatars to close #169

--- Dockerfile
+++ Dockerfile
@@ -53,6 +53,7 @@ RUN pip install 'django-hstore==1.3.5'
 RUN pip install cognitiveatlas
 RUN pip install django-datatables-view
 RUN pip install 'django-oauth-toolkit==0.9.0'
+RUN pip install django-gravatar2
 
 RUN mkdir /code
 WORKDIR /code
--- expdj/apps/main/urls.py
+++ expdj/apps/main/urls.py
@@ -5,7 +5,6 @@ from .views import index_view, signup_view, about_view, get_token, \
 
 urlpatterns = patterns('',
     url(r'^$', index_view, name="index"),
-    url(r'^signup$', signup_view, name="signup"),
     url(r'^search$', search_view, name="search"),
     url(r'^token$', get_token, name="get_token"),
     url(r'^batteries/(?P<bid>\d+|[A-Z]{8})/(?P<keyid>\d+|[A-Za-z0-9-]{32})/auth$',google_auth_view,name='google_auth_view'), # use google for auth
